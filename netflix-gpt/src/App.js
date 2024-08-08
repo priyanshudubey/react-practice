cb4914065a791a04e63fde2e8ba8d9b6d73e9a42
priyanshudubey <priyanshu0dubey@gmail.com>
2024-08-08 21:00:16 +0100

    adding netflix-gpt project setup

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-bold text-3xl text-orange-700">Netflix-GPT</h1>
+    </div>
+  );
+}
+
+export default App;
