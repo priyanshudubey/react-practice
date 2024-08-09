a25e1ace2e704945394ce23ea624ad12edc07bee
priyanshudubey <priyanshu0dubey@gmail.com>
2024-08-09 22:27:57 +0100

    form validation and firebase setup

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+  };
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
@@ -15,7 +25,9 @@ const Login = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/b2c3e95b-b7b5-4bb7-a883-f4bfc7472fb7/c47eb5b8-07f7-400e-9d54-49630857116d/GB-en-20240805-POP_SIGNUP_TWO_WEEKS-perspective_WEB_37d05758-e321-4b45-a195-519d45729021_large.jpg"
         />
       </div>
-      <form className="absolute m-36 p-12 w-3/12 mx-auto right-0 left-0 text-white bg-opacity-80 bg-black">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute m-36 p-12 w-3/12 mx-auto right-0 left-0 text-white bg-opacity-80 bg-black">
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -27,16 +39,21 @@ const Login = () => {
           />
         )}
         <input
-          type="text"
+          ref={email}
+          type="email"
           placeholder="Email Address"
           className="p-2 my-4 w-full rounded-lg bg-gray-900"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-2 my-4 w-full rounded-lg bg-gray-900"
         />
-        <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <button
+          className="p-4 my-4 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}>
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p
@@ -46,6 +63,13 @@ const Login = () => {
             ? "New to Netflix? Sign up now."
             : "Already a user? Sign In now"}
         </p>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            className="m-4 p-4 w-4 h-4 rounded-lg"
+          />
+          <span>Remember me</span>
+        </label>
       </form>
     </div>
   );
