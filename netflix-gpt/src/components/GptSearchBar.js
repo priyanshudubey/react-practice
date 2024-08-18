2bea854e8b56c7eb55d573fed320c77f63e890a7
priyanshudubey <priyanshu0dubey@gmail.com>
2024-08-18 23:01:16 +0100

    added multilingual feature in gpt search bar

--- netflix-gpt/src/components/GptSearchBar.js
+++ netflix-gpt/src/components/GptSearchBar.js
@@ -1,19 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
 
 const GptSearchBar = () => {
+  const langKey = useSelector((store) => store.config.lang);
   return (
     <div className="pt-[15%] flex justify-center">
       <form className=" bg-black w-1/2 grid grid-cols-12 rounded-lg">
         <input
           type="text"
           className="p-4 m-4 col-span-9"
-          placeholder={lang.hindi.gptSearchPlaceholder}
+          placeholder={lang[langKey].gptSearchPlaceholder}
         />
         <button
           type="submit"
           className="py-2 px-4 bg-red-700 text-white col-span-3 m-4">
-          {lang.hindi.search}
+          {lang[langKey].search}
         </button>
       </form>
     </div>
--- netflix-gpt/src/components/Header.js
+++ netflix-gpt/src/components/Header.js
@@ -7,6 +7,8 @@ import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
 import { APP_LOGO } from "../utils/constants";
 import { toogleGptSearchView } from "../utils/gptSlice";
+import { SUPPORTED_LANGUAGES } from "../utils/constants";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -18,7 +20,6 @@ const Header = () => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName, photoURL } = user;
-        // console.log("User ID: ", user);
         dispatch(
           addUser({
             uid: uid,
@@ -49,6 +50,12 @@ const Header = () => {
         // An error happened.
       });
   };
+
+  const handleLanguageChange = (e) => {
+    // console.log(e.target.value);
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
@@ -58,10 +65,16 @@ const Header = () => {
       />
       {user && (
         <div className="flex items-center p-2">
-          <select>
-            <option value="en">English</option>
-            <option vlaue="hindi">Spanish</option>
-            <option value="spanish">Hindi</option>
+          <select
+            className="p-2 bg-gray-900 text-white m-2"
+            onChange={handleLanguageChange}>
+            {SUPPORTED_LANGUAGES.map((lang) => (
+              <option
+                key={lang.identifier}
+                value={lang.identifier}>
+                {lang.name}
+              </option>
+            ))}
           </select>
           <img
             className="w-12 h-12 rounded-sm mr-4"
